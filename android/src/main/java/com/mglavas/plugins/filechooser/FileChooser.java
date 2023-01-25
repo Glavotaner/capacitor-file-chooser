9ba387c30a0716aaef77fb0154d3a601aaa54c3a
Marin Glavaš <marin.glavas@spin.hr>
2023-01-25 15:31:29 +0100

    Initial commit

---
+++ android/src/main/java/com/mglavas/plugins/filechooser/FileChooser.java
@@ -0,0 +1,11 @@
+package com.mglavas.plugins.filechooser;
+
+import android.util.Log;
+
+public class FileChooser {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
